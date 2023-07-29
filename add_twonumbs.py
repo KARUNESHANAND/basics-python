36a2e282acb26ce164b24714a8ace21ac8701bc4
Karunesh Anand <83410805+KARUNESHANAND@users.noreply.github.com>
2023-07-29 19:42:51 +0530

    Add files via upload

---
+++ add_twonumbs.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+num_0 = int(two_digit_number[0])
+num_1 = int(two_digit_number[1])
+sum = num_0 + num_1
+print(sum)
+####################################
+
+#Write your code below this line 👇
